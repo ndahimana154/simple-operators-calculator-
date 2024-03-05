f23d0b37b8c32d42cd9bb78456472b450cf9c923
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-03-05 08:18:40 +0200

    FInished redoing the Logics behing the JS

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,45 +1,10 @@
-var chechkButton = document.getElementById("checkResult");
-
-chechkButton.addEventListener("click", function () {
-  var num1 = document.getElementById("number1").value;
-  var operatorValue = document.getElementById("operator").value;
-  var num2 = document.getElementById("number2").value;
-  var resultsBox = document.getElementById("results");
-var answer
-  var errorsArray = [];
-  if (operatorValue == "Select Operator") {
-    errorsArray.push(`Please select Operator`);
+resultField = document.getElementById("displayField");
+const deleteValues = () => {
+  resultsValues = resultField.value
+  rvArray = resultsValues.split('')
+  newArray = ""
+  for (let i = 0;i<rvArray.length - 1;i++) {
+    newArray += rvArray[i]
   }
-  if (num1 == false || num2 == false) {
-    errorsArray.push(`Error in your numbers`);
-  }
-  if (errorsArray.length > 0) {
-    resultsBox.textContent = errorsArray
-  }
-  else {
-    num1Value = parseInt(num1)
-    num2Value = parseInt(num2)
-    switch (operatorValue) {
-        case '+':
-            answer = num1Value + num2Value
-            break;
-        case '-':
-            answer = num1Value - num2Value
-            break;
-        case '*':
-            answer = num1Value * num2Value
-            break;
-        case '/':
-            answer = num1Value / num2Value
-            break;
-        case '**':
-            answer = num1Value ** num2Value
-            break;
-    
-        default:
-            answer = `Invalid Operator`
-            break;
-    }
-    resultsBox.textContent=`${num1Value} ${operatorValue} ${num2Value} = ${answer}`
-  }
-});
+  resultField.value = newArray
+}
